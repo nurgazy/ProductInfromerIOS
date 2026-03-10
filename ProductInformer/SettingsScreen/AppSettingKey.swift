86f9e07868c832a96530aa51b92dba79d1f27e54
nurgazy <megalifekg@gmail.com>
2026-03-10 21:26:57 +0600

    addded cyclic scanning

---
+++ ProductInformer/SettingsScreen/AppSettingKey.swift
@@ -0,0 +1,13 @@
+import Foundation
+import SwiftUI
+
+public struct AppSettingKey {
+    static let protocolSelection = "protocolSelectionKey"
+    static let serverAddress = "serverAddressKey"
+    static let port = "portKey"
+    static let publicationName = "publicationNameKey"
+    static let username = "usernameKey"
+    static let password = "passwordKey"
+    static let isFullSpecific = "isFullSpecificKey"
+    static let isCyclicScanning = "isCyclicScanningKey"
+}
